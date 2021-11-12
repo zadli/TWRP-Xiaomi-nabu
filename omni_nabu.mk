ca73b1a79d2d5923d5a8f79be5ae237000464c92
mujianwu <62330315+mujianwu@users.noreply.github.com>
2021-11-12 23:35:17 +0800

    Update omni_nabu.mk

--- omni_nabu.mk
+++ omni_nabu.mk
@@ -6,6 +6,7 @@
 #
 
 # Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
@@ -15,7 +16,7 @@ $(call inherit-product, device/xiaomi/nabu/device.mk)
 
 # Inherit some common Omni stuff.
 $(call inherit-product, vendor/omni/config/common.mk)
-$(call inherit-product, vendor/omni/config/gsm.mk)
+#$(call inherit-product, vendor/omni/config/gsm.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := nabu
