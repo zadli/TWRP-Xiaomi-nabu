bc045832fc3c0ad526b2edfd6d57a124d5b4977f
mujianwu <62330315+mujianwu@users.noreply.github.com>
2021-11-12 23:36:33 +0800

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_nabu-user
-add_lunch_combo omni_nabu-userdebug
-add_lunch_combo omni_nabu-eng
+#add_lunch_combo omni_nabu-user
+#add_lunch_combo omni_nabu-userdebug
+#add_lunch_combo omni_nabu-eng
